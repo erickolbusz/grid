b5db4c05470d31fbb98f6a43c4b9235d9d459525
erickolbusz <erickolbusz@gmail.com>
2017-05-30 23:00:39 -0400

    Bugfix in carnivore movement

--- Carnivore.java
+++ Carnivore.java
@@ -31,7 +31,7 @@ public class Carnivore extends Animal {
 	public void growOlder() {
 		age++;
 		if (age <= 10) {
-			visionRadius += 2;
+			visionRadius += 1;
 			//visionScale *= 0.8;
 		}
 		else {
@@ -56,6 +56,7 @@ public class Carnivore extends Animal {
 				if (isHungry()) {
 					//try to find a herbivore nearby to eat
 					boolean hunting = hunt(grid);
+					//System.out.println(hunting+"\n");
 					if (!hunting) {
 						//didn't find prey nearby
 						moveRandomly(grid);
@@ -116,6 +117,7 @@ public class Carnivore extends Animal {
 			}
 		}
 		if (closestHerbivoreX != 0 || closestHerbivoreY != 0) {
+			//System.out.println(minDistSq+"\n");
 			//nearby prey exists
 			if (closestHerbivoreX >= -1 && closestHerbivoreX <= 1 && closestHerbivoreY >= -1 && closestHerbivoreY <= 1) {
 				//adjacent prey
@@ -150,11 +152,13 @@ public class Carnivore extends Animal {
 				}
 			}
 
+			final int herbX = closestHerbivoreX;
+			final int herbY = closestHerbivoreY;
 			Arrays.sort(moves, new Comparator<int[]>() {
 			    @Override
 			    public int compare(int[] move1, int[] move2) {
-					double d1 = Math.pow(x-move1[0],2) + Math.pow(y-move1[1],2);
-					double d2 = Math.pow(x-move2[0],2) + Math.pow(y-move2[1],2);
+					double d1 = Math.pow(herbX-move1[0],2) + Math.pow(herbY-move1[1],2);
+					double d2 = Math.pow(herbX-move2[0],2) + Math.pow(herbY-move2[1],2);
 					if (d1 < d2) {
 						return -1;
 					}
