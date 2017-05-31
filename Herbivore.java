68772291676657b41df1ce443eb0914957bc7fcf
James Wong <jameswong8@gmail.com>
2017-05-30 23:45:05 -0400

    commit

--- Herbivore.java
+++ Herbivore.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class Herbivore extends Animal {
 	private Herbivore prevHerb, nextHerb; // following herbivore
@@ -56,24 +57,21 @@ public class Herbivore extends Animal {
 				if(prevHerb != null) {
 					nextHerb.setPrevHerb(prevHerb);
 					prevHerb.setNextHerb(nextHerb);
-				} else { // firstHerb
+				} else if(prevHerb == null) { // firstHerb
 					Herder.getInstance().setFirstHerb(nextHerb);
 				}
 				nextHerb.move(grid, X, Y);
-			} else { // no nextHerb
+			} else if(nextHerb == null) { // no nextHerb
 				if(prevHerb != null) {
 					prevHerb.setNextHerb(null);
-				} else { // no more herbs
+				} else if(prevHerb == null) { // no more herbs
 					//Herder.getInstance().setFirstHerb(null);
-					System.out.println("GAME OVER\n\n\n\n\n\n\n\n");
+					//JOptionPane.showMessageDialog(null, "All of your herbivores died. Game Over!", "GAME OVER!", JOptionPane.WARNING_MESSAGE);
 				}
 			}
 		}
 	}
     			
-    		
-		
-
 	public void move(Entity[][] grid, int newX, int newY) {
 		// following what is before int
 		int X = this.getX();
@@ -115,85 +113,5 @@ public class Herbivore extends Animal {
 				}
 			}
 		}
-
-		// if (!isBirthing) {
-		// 	//if hungry and not giving birth, try to find a plant nearby to eat
-		// 	boolean isHunting = false;
-		// 	if (isHungry()) {
-		// 		for (int dx = -1; dx <= 1; dx++) {
-		// 			for (int dy = -1; dy <= 1; dy++) {
-		// 				try {
-		// 					if (grid[x+dx][y+dy] instanceof Plant && !isHunting) {
-		// 						//found adjacent plant
-		// 						isHunting = true;
-		// 						Plant p = (Plant)grid[x+dx][y+dy];
-
-		// 						eat(p);
-		// 						grid[x][y] = null;
-		// 						grid[x+dx][y+dy] = this;
-		// 						x = x+dx;
-		// 						x = y+dy;
-		// 					}
-		// 				} catch(ArrayIndexOutOfBoundsException e) {
-		// 					//off the grid
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
 	}
-	// @Override
-	// public void move(Entity[][] grid) {
-	// 	//checks to see if the herbivore can give birth, else it moves
-	// 	boolean isBirthing = false;
-	// 	if (age >= 5 && age <= 12 && energy >= 9) {
-	// 		//needs an empty adjacent space to spawn a new herbivore
-	// 		// for (int dx = -1; dx <= 1; dx++) {
-	// 		// 	for (int dy = -1; dy <= 1; dy++) {
-	// 				try {
-	// 					if (grid[x+dx][y+dy] == null && !isBirthing) {// if (grid[x+dx][y+dy] == null && !isBirthing) {
-	// 						//give birth
-	// 						isBirthing = true;
-	// 						energy = energy-4;
-	// 						grid[x+dx][y+dy] = new Herbivore(x+dx,y+dy);
-	// 						//newborn cannot move this cycle
-	// 						((Animal)grid[x+dx][y+dy]).startMoving();
-	// 					}
-	// 				} catch(ArrayIndexOutOfBoundsException e) {
-	// 					//off the grid
-	// 				}
-	// 		// 	}
-	// 		// }
-	// 	}
-
-	// 	if (!isBirthing) {
-	// 		//if hungry and not giving birth, try to find a plant nearby to eat
-	// 		boolean isHunting = false;
-	// 		if (isHungry()) {
-	// 			for (int dx = -1; dx <= 1; dx++) {
-	// 				for (int dy = -1; dy <= 1; dy++) {
-	// 					try {
-	// 						if (grid[x+dx][y+dy] instanceof Plant && !isHunting) {
-	// 							//found adjacent plant
-	// 							isHunting = true;
-	// 							Plant p = (Plant)grid[x+dx][y+dy];
-
-	// 							eat(p);
-	// 							grid[x][y] = null;
-	// 							grid[x+dx][y+dy] = this;
-	// 							x = x+dx;
-	// 							x = y+dy;
-	// 						}
-	// 					} catch(ArrayIndexOutOfBoundsException e) {
-	// 						//off the grid
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 		// if (!isHunting) {
-	// 		// 	//didn't find food nearby
-	// 		// 	moveRandomly(grid);
-	// 		// }
-	// 	}
-	// }
 }
